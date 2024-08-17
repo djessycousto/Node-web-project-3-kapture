5ae15cb569db94e19a55fdbe328093677372b02a
djessycousto <79341027+djessycousto@users.noreply.github.com>
2024-08-18 00:59:54 +0100

    address updated  resized Md fixed SEO optimised

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,22 @@ import { slideShow, tiltImg } from "./carousel.js";
 import { menuAnim } from "./simpleAnim.js";
 import { contactForm } from "./contact.js";
 // import { test } from "./test.js";
+
+// let lastWidth = window.innerWidth;
+
+// window.addEventListener("resize", function () {
+//   // Get the current window width
+//   let currentWidth = window.innerWidth;
+
+//   // Check if we've crossed the 450px breakpoint
+//   if (
+//     (lastWidth < 450 && currentWidth >= 450) ||
+//     (lastWidth >= 450 && currentWidth < 450)
+//   ) {
+//     // Refresh the page
+//     window.location.reload();
+//   }
+
+//   // Update lastWidth with the current window width
+//   lastWidth = currentWidth;
+// });
--- public/js/main-js.js
+++ public/js/main-js.js
@@ -38,6 +38,46 @@ export function serviceToggle() {
   });
 }
 
+// Reset the page when the window is resized  to correct my MD issue
+// let lastWidth = window.innerWidth;
+
+// window.addEventListener("resize", function () {
+//   // Get the current window width
+//   let currentWidth = window.innerWidth;
+
+//   // Check if we've crossed the 450px breakpoint
+//   if (
+//     (lastWidth < 670 && currentWidth >= 670) ||
+//     (lastWidth >= 670 && currentWidth < 670)
+//   ) {
+//     // Refresh the page
+//     window.location.reload();
+//   }
+
+//   // Update lastWidth with the current window width
+//   lastWidth = currentWidth;
+// });
+
+let lastWidth = window.innerWidth;
+
+window.addEventListener("resize", function () {
+  // Get the current window width
+  let currentWidth = window.innerWidth;
+
+  // Check if we've crossed the 450px breakpoint
+  if (
+    (lastWidth < 670 && currentWidth >= 670) ||
+    (lastWidth >= 670 && currentWidth < 670)
+  ) {
+    // Refresh the page with a unique query string to prevent caching
+    window.location.href =
+      window.location.href.split("?")[0] + "?refreshed=" + new Date().getTime();
+  }
+
+  // Update lastWidth with the current window width
+  lastWidth = currentWidth;
+});
+
 serviceToggle();
 
 // section 3 slideShow
